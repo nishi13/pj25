62ef7212ad567414b3a643270978f4f1f5d0b273
Pedro Martinez <pedromartinez@Pedros-MacBook-Pro.local>
2014-04-26 19:28:51 -0300

    views + urls

--- sistema/views.py
+++ sistema/views.py
@@ -1,1 +1,15 @@
-# Create your views here.
+# imports
+from django.db import models
+from django import forms
+from sistema.forms import *
+from sistema.models import *
+from django.shortcuts import render_to_response, get_object_or_404, get_list_or_404, redirect
+
+# functions
+def home (request):
+
+	return HttpResponseRedirect('/egplus')
+
+def egplus (request):
+
+	return render_to_response ('egplus.html', locals(), context_instance = RequestContext(request))
--- urls.py
+++ urls.py
@@ -1,12 +1,17 @@
 from django.conf.urls.defaults import *
+from django.conf import settings
+import django.contrib.auth.views
+from django.contrib import admin
+import os
+admin.autodiscover()
 
 # Uncomment the next two lines to enable the admin:
 # from django.contrib import admin
 # admin.autodiscover()
 
 urlpatterns = patterns('',
-    # Example:
-    # (r'^pj25/', include('pj25.foo.urls')),
+	(r'','sistema.views.home'),
+	(r'^egplus/$','sistema.views.egplus'),
 
     # Uncomment the admin/doc line below to enable admin documentation:
     # (r'^admin/doc/', include('django.contrib.admindocs.urls')),
