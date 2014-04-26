986f86634d2da9564f8b09388984c280dcd24abf
nishi13 <augusto.nishi@gmail.com>
2014-04-26 20:34:59 -0300

    Add scripts, to ./run to run server and ./sync to syncdb

--- sistema/models.py
+++ sistema/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
 # Create your models here.
+
+class Cargo(models.Model):
+    nome = models.CharField(max_length=64, null=True, blank=True, verbose_name="Cargo / Ultimo Cargo")
+
+    def __unicode__(self):
+        return self.nome
+        
+class Membros(models.Model):
+    
+    nome = models.CharField(max_length=128, verbose_name="Nome Completo")
+    apelido = models.CharField(max_length=64, null=True, blank=True, verbose_name="Apelido")
+    ano_de_ingresso = models.IntegerField(null=True, blank=True, verbose_name="Ano de Ingresso")
+    ano_de_saida = models.IntegerField(null=True, blank=True, verbose_name="Ano de Saida")
+    email = models.EmailField(max_length=128, verbose_name="E-mail")
+    cargo = models.ForeignKey(Cargo)
+    
+    def __unicode__(self):
+        return self.nome
