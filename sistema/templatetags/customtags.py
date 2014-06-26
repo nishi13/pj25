200023edc25f71bca10b7b04c375effb9b6c8cbc
nishi13 <augusto.nishi@gmail.com>
2014-06-26 18:32:58 -0300

    fixed some security issues

--- sistema/templatetags/customtags.py
+++ sistema/templatetags/customtags.py
@@ -5,6 +5,6 @@ register = template.Library()
 
 def safe_tags(value):
     """Returns the given HTML with all tags stripped."""
-    return re.sub(r'<(?!/?(p|span|(h+[0-9])|style|div|br|hr))[^>]*?>', '', unicode(value))
+    return re.sub(r'<(?!/?(p|span|(h+[0-9])|style|div|br|hr|font))[^>]*?>', '', unicode(value))
 safe_tags.is_safe = True
 register.filter('safe_tags', safe_tags)
